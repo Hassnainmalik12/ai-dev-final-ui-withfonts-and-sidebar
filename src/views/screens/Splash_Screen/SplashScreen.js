e1c7fd908fb9633a63b0c42254b61d89837b1eb4
Ali Hassnain <hassnainmalik726@gmail.com>
2024-08-24 13:25:51 +0500

    splash screen work done

--- src/views/screens/Splash_Screen/SplashScreen.js
+++ src/views/screens/Splash_Screen/SplashScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { navigateToScreen } from "../../../utils/nav_controller";
 import { SCREEN } from "../../../models/constants/screen";
 import splashGif from '../../../images/preloader.gif';
-import animatedImage from '../../../images/arrow.png'; // Replace with your image path
+import animatedImage from '../../../images/Splash 2.png'; // Replace with your image path
 
 const SplashScreen = () => {
     const [text, setText] = useState('Ai dev is coming.');
@@ -11,44 +11,44 @@ const SplashScreen = () => {
 
     useEffect(() => {
         const firstTimer = setTimeout(() => {
-            setText('Ai dev is coming..');
-        }, 500);
+            setText('Ai dev is coming ..');
+        }, 400);
 
         const secondTimer = setTimeout(() => {
-            setText('Ai dev is coming...');
-        }, 1000);
+            setText('Ai dev is coming ...');
+        }, 800);
 
         const thirdTimer = setTimeout(() => {
-            setText('Ai dev is coming...');
-        }, 1500);
+            setText('Ai dev is coming ...');
+        }, 1200);
 
         const fourthTimer = setTimeout(() => {
-            setText('Ai dev is coming....');
-        }, 2100);
+            setText('Ai dev is coming ....');
+        }, 1600);
 
         const animateTimer = setTimeout(() => {
             setAnimate(true);
-        }, 2100);
+        }, 1900);
 
         const navigateTimer = setTimeout(() => {
-            setLoaded(true);
-        }, 2700);
+            navigateToScreen(SCREEN.MAIN);
+        }, 2400);
 
         return () => {
             clearTimeout(firstTimer);
             clearTimeout(secondTimer);
             clearTimeout(thirdTimer);
             clearTimeout(fourthTimer);
-            clearTimeout(animateTimer);
+            // clearTimeout(animateTimer);
             clearTimeout(navigateTimer);
         };
     }, []);
 
-    useEffect(() => {
-        if (loaded) {
-            navigateToScreen(SCREEN.MAIN);
-        }
-    }, [loaded]);
+    // useEffect(() => {
+    //     if (loaded) {
+    //         navigateToScreen(SCREEN.MAIN);
+    //     }
+    // }, [loaded]);
 
     return (
         <div className={`splash ${animate ? 'animate-up' : ''}`} onClick={event => navigateToScreen(SCREEN.APP)}>
